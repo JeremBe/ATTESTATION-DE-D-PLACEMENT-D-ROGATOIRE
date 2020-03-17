ade1914c1004ef1baecc1e167309969ef368d073
jeremy bouche <j.bouche33@gmail.com>
2020-03-17 15:06:14 +0100

    Updates

---
+++ precache-manifest.55d6d03af09fb4e0ba5a5dade55af9cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d177b39a5864ba5837f22f2d7ec2c87",
+    "url": "/JeremBe/ATTESTATION-DE-D-PLACEMENT-D-ROGATOIRE/tree/gh-pages/index.html"
+  },
+  {
+    "revision": "83246aa1f8676473ef26",
+    "url": "/JeremBe/ATTESTATION-DE-D-PLACEMENT-D-ROGATOIRE/tree/gh-pages/static/css/main.6f96dd25.chunk.css"
+  },
+  {
+    "revision": "27566d82ca94108019b5",
+    "url": "/JeremBe/ATTESTATION-DE-D-PLACEMENT-D-ROGATOIRE/tree/gh-pages/static/js/2.8860c6a6.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/JeremBe/ATTESTATION-DE-D-PLACEMENT-D-ROGATOIRE/tree/gh-pages/static/js/2.8860c6a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83246aa1f8676473ef26",
+    "url": "/JeremBe/ATTESTATION-DE-D-PLACEMENT-D-ROGATOIRE/tree/gh-pages/static/js/main.02052345.chunk.js"
+  },
+  {
+    "revision": "b7ea236b8a5f68dc4643",
+    "url": "/JeremBe/ATTESTATION-DE-D-PLACEMENT-D-ROGATOIRE/tree/gh-pages/static/js/runtime-main.5d648733.js"
+  },
+  {
+    "revision": "018d7708c10d8d08c78ce5474f79ba05",
+    "url": "/JeremBe/ATTESTATION-DE-D-PLACEMENT-D-ROGATOIRE/tree/gh-pages/static/media/signature.018d7708.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/JeremBe/ATTESTATION-DE-D-PLACEMENT-D-ROGATOIRE/precache-manifest.c8d53fe96c49eaf6d5b3292fd2404f11.js"
+  "/JeremBe/ATTESTATION-DE-D-PLACEMENT-D-ROGATOIRE/tree/gh-pages/precache-manifest.55d6d03af09fb4e0ba5a5dade55af9cd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/JeremBe/ATTESTATION-DE-D-PLACEMENT-D-ROGATOIRE/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/JeremBe/ATTESTATION-DE-D-PLACEMENT-D-ROGATOIRE/tree/gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
