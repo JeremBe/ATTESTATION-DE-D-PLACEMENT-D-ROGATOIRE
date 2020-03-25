f169dac50338ec5e411f9cd713e672ddcc2d5df2
jeremy bouche <j.bouche33@gmail.com>
2020-03-25 12:15:25 +0100

    j

--- src/components/page.js
+++ src/components/page.js
@@ -3,7 +3,7 @@ import Document from "./Document";
 import { PDFViewer } from "@react-pdf/renderer";
 
 const Page = function({ form }) {
-  console.log("test git slack");
+  console.log("test git slacke");
   return (
     <PDFViewer className="fw fh">
       <Document form={form} />
